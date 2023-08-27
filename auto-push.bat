703f9e2f0b05635125a346a1167d348b483542ac
Xin-FAS <xin.fas@outlook.com>
2023-08-27 22:52:17 +0800

    update

--- auto-push.bat
+++ auto-push.bat
@@ -4,7 +4,7 @@ call git clone git@github.com:Xin-FAS/xin-fas.github.io.git
 if exist xin-fas.github.io\.git\ (
     call xcopy xin-fas.github.io\.git\ .\.git\ /s/q
     call rd xin-fas.github.io /s/q
-    call if exist public\iframe\ (
+    if exist public\iframe\ (
         call rd public\iframe /s/q
     )
     call xcopy source\html\ public\iframe\ /s/q
@@ -16,6 +16,6 @@ if exist xin-fas.github.io\.git\ (
     call rd .git /s/q
     pause
 ) else (
-    echo "git拉取失败！"
+    echo "git clone error!"
     pause
 )
