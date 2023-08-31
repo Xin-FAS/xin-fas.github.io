768c65a47c21cbc03597360d549409410c6fe758
Xin-FAS <xin.fas@outlook.com>
2023-08-31 23:24:50 +0800

    update

--- auto-push.bat
+++ auto-push.bat
@@ -1,12 +1,12 @@
 @echo off
+if exist public\ (
+    call rd public /s/q
+)
 call hexo g
 call git clone git@github.com:Xin-FAS/xin-fas.github.io.git
 if exist xin-fas.github.io\.git\ (
     call xcopy xin-fas.github.io\.git\ .\.git\ /s/q
     call rd xin-fas.github.io /s/q
-    if exist public\iframe\ (
-        call rd public\iframe /s/q
-    )
     call xcopy source\html\ public\iframe\ /s/q
     call xcopy public\ docs\ /s/q
     call git add .
